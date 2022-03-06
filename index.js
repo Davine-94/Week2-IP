30b1fa1c237a3b5fddc8846ee2fd7f8f03dee8ad
davine94 <davine94>
2022-03-06 20:16:44 +0300

    deleted the if statements on my js file

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
 const arrayMale= ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi', 'Kwame'];
 const arrayFemale=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
 
-dInput= "birthday";
-mInput="male";
-fInput="female";
 
-
-function birthdate()
-    {
-    let dInput= document.getElementById("birthday").value;
-    let mInput= document.getElementById("male");
-    let fInput= document.getElementById("female");
-}
-
-if(dInput == '') {
-    alert("please enter a valid birth value");
-}
-if(mInput == '') {
-    alert("please select gender");
-}
-if (dInput == '') {
-    alert("please select gender");
-}
-
-date= new Date(dInput);
-console.log(date);
